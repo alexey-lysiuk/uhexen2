45de94e8cd51e34f9f1bb8ce6f1c9a64b0d316fc
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-12-17 15:39:40 +0200

    * replace SDL framework with static library
    SDL 1.2.x i386/x64 static library @ b1d2398f2655
    
    https://www.libsdl.org/
    http://hg.libsdl.org/SDL/archive/b1d2398f2655.tar.bz2
    
    Built on OS X 10.6 using Xcode 3.2.6 with X11 video driver disabled

--- oslibs/macosx/include/SDL_config.h
+++ oslibs/macosx/include/SDL_config.h
@@ -20,31 +20,27 @@
     slouken@libsdl.org
 */
 
-#ifndef _SDL_config_macos_h
-#define _SDL_config_macos_h
+#ifndef _SDL_config_macosx_h
+#define _SDL_config_macosx_h
 
 #include "SDL_platform.h"
 
+/* This gets us MAC_OS_X_VERSION_MIN_REQUIRED... */
+#include <AvailabilityMacros.h>
+
 /* This is a set of defines to configure the SDL features */
-
-#include <MacTypes.h>
-
-typedef SInt8	int8_t;
-typedef UInt8	uint8_t;
-typedef SInt16	int16_t;
-typedef UInt16	uint16_t;
-typedef SInt32	int32_t;
-typedef UInt32	uint32_t;
-typedef SInt64	int64_t;
-typedef UInt64	uint64_t;
-typedef unsigned long	uintptr_t;
 
 #define SDL_HAS_64BIT_TYPE	1
 
 /* Useful headers */
+/* If we specified an SDK or have a post-PowerPC chip, then alloca.h exists. */
+#define HAVE_ALLOCA_H		1
+#define HAVE_SYS_TYPES_H	1
 #define HAVE_STDIO_H	1
 #define STDC_HEADERS	1
 #define HAVE_STRING_H	1
+#define HAVE_INTTYPES_H	1
+#define HAVE_STDINT_H	1
 #define HAVE_CTYPE_H	1
 #define HAVE_MATH_H	1
 #define HAVE_SIGNAL_H	1
@@ -55,58 +51,73 @@ typedef unsigned long	uintptr_t;
 #define HAVE_REALLOC	1
 #define HAVE_FREE	1
 #define HAVE_ALLOCA	1
+#define HAVE_GETENV	1
+#define HAVE_PUTENV	1
+#define HAVE_UNSETENV	1
+#define HAVE_QSORT	1
 #define HAVE_ABS	1
+#define HAVE_BCOPY	1
 #define HAVE_MEMSET	1
 #define HAVE_MEMCPY	1
 #define HAVE_MEMMOVE	1
 #define HAVE_MEMCMP	1
 #define HAVE_STRLEN	1
+#define HAVE_STRLCPY	1
+#define HAVE_STRLCAT	1
+#define HAVE_STRDUP	1
 #define HAVE_STRCHR	1
 #define HAVE_STRRCHR	1
 #define HAVE_STRSTR	1
-#define HAVE_ITOA	1
 #define HAVE_STRTOL	1
+#define HAVE_STRTOUL	1
+#define HAVE_STRTOLL	1
+#define HAVE_STRTOULL	1
 #define HAVE_STRTOD	1
 #define HAVE_ATOI	1
 #define HAVE_ATOF	1
 #define HAVE_STRCMP	1
 #define HAVE_STRNCMP	1
+#define HAVE_STRCASECMP	1
+#define HAVE_STRNCASECMP 1
 #define HAVE_SSCANF	1
+#define HAVE_SNPRINTF	1
+#define HAVE_VSNPRINTF	1
+#define HAVE_SIGACTION	1
+#define HAVE_SETJMP	1
+#define HAVE_NANOSLEEP	1
 
 /* Enable various audio drivers */
-#define SDL_AUDIO_DRIVER_SNDMGR	1
+#define SDL_AUDIO_DRIVER_COREAUDIO	1
 #define SDL_AUDIO_DRIVER_DISK	1
 #define SDL_AUDIO_DRIVER_DUMMY	1
 
 /* Enable various cdrom drivers */
-#if TARGET_API_MAC_CARBON
-#define SDL_CDROM_DUMMY		1
-#else
-#define SDL_CDROM_MACOS		1
-#endif
+#define SDL_CDROM_MACOSX	1
 
 /* Enable various input drivers */
-#if TARGET_API_MAC_CARBON
-#define SDL_JOYSTICK_DUMMY	1
-#else
-#define SDL_JOYSTICK_MACOS	1
-#endif
+#define SDL_JOYSTICK_IOKIT	1
 
 /* Enable various shared object loading systems */
-#define SDL_LOADSO_MACOS	1
+#define SDL_LOADSO_DLOPEN	1
 
 /* Enable various threading systems */
-#define SDL_THREADS_DISABLED	1
+#define SDL_THREAD_PTHREAD	1
+#define SDL_THREAD_PTHREAD_RECURSIVE_MUTEX	1
 
 /* Enable various timer systems */
-#define SDL_TIMER_MACOS	1
+#define SDL_TIMER_UNIX	1
 
 /* Enable various video drivers */
 #define SDL_VIDEO_DRIVER_DUMMY	1
-#define SDL_VIDEO_DRIVER_DRAWSPROCKET	1
-#define SDL_VIDEO_DRIVER_TOOLBOX	1
+#define SDL_VIDEO_DRIVER_QUARTZ	1
 
 /* Enable OpenGL support */
 #define SDL_VIDEO_OPENGL	1
 
-#endif /* _SDL_config_macos_h */
+/* Disable screensaver */
+#define SDL_VIDEO_DISABLE_SCREENSAVER	1
+
+/* Enable assembly routines */
+#define SDL_ASSEMBLY_ROUTINES	1
+
+#endif /* _SDL_config_macosx_h */
